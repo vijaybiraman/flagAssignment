3f2f61d80972c2aa2c2cd3c153c660f6fca7f25f
vijaybiraman <109381804+vijaybiraman@users.noreply.github.com>
2022-08-15 23:12:39 +0530

    Add files via upload

---
+++ Dataprovider.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+namespace FlagLibraryDemo
+{
+    public class Dataprovider
+    {
+        SqlConnection connection = null;
+        SqlCommand command = null;
+        SqlDataReader reader = null;
+        public  Dataprovider(string connectionString)
+        {
+            connection = new SqlConnection(connectionString);
+        }
+        public List<Continent> getCountryflag(string name)
+        {
+            try
+            {
+                string query = "select * from country where countryname=@country";
+                command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("country", name);
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+                reader = command.ExecuteReader();
+                List<Continent> list = new List<Continent>();
+                while(reader.Read())
+                {
+                    Continent continent = new Continent();
+                    continent.countryId = reader["countryId"].ToString();
+                    continent.capital = reader["capital"].ToString();
+                    continent.flag = (byte[])(reader["flag"]);
+                    list.Add(continent);
+                }
+                return list;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+        }
+        public List<Continent> getState(string id)
+        {
+            try
+            {
+                string query = "select * from state where countryid=@id";
+                command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("id", id);
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+                reader = command.ExecuteReader();
+                List<Continent> list = new List<Continent>();
+                    while (reader.Read())
+                    {
+                        Continent continent = new Continent();
+                        continent.statename = reader["statename"].ToString();
+                        list.Add(continent);
+                    }
+                    return list;               
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+        }
+        public string getcapital(string name)
+        {
+            try
+            {
+                string query = "select statecapital from state where statename=@name";
+                command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("name", name);
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+                reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    Continent continent = new Continent();
+                    while (reader.Read())
+                    {
+                        continent.statecapital = reader["statecapital"].ToString();
+                    }
+                    return continent.statecapital;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FlagLibraryDemo;
+using System.Configuration;
+namespace WpfflagDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       Dataprovider dataprovider = null;
+      static int count=0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            dataprovider = new Dataprovider(ConfigurationManager.ConnectionStrings["connstr"].ConnectionString);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtcountry.Text;
+             string id=null;
+            List<Continent>list=dataprovider.getCountryflag(name);
+            foreach(var i in list)
+            {
+                txtcapital.Text = i.capital;
+                id = i.countryId;
+                getflag.DataContext = i.flag;
+            }
+            List<Continent> list2 = dataprovider.getState(id);
+            if (count == 0)
+            {
+                foreach (var i in list2)
+                {
+                    txtcity.Items.Add(i.statename);
+                }
+                count++;
+            }
+             string nm = dataprovider.getcapital(txtcity.Text);
+            txtcapital1.Text = nm;
+        }
+        private void Mainlayout_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+        }
+    }
+}
