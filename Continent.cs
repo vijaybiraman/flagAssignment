cbee721dc14fc0185c8833bfb9467f2e0d653877
vijaybiraman <109381804+vijaybiraman@users.noreply.github.com>
2022-08-15 23:13:37 +0530

    Add files via upload

---
+++ Continent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlagLibraryDemo
+{
+    public class Continent
+    {
+        public string countryId { get; set; }
+        public string capital { get; set; }
+        public byte[] flag { get; set; }
+        public string statename { get; set; }
+        public string statecapital { get; set; }
+    }
+}
